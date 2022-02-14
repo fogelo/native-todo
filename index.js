e1e10d40c0fe2a9cdc78b6afea571dd72508379b
antonorlov <fogelo@yandex.ru>
2022-02-14 17:02:40 +0300

    добавил валидацию для поля ввода, opacity для завершенных задач и привязал чекбокс к isDone

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let activeBtnFilter = document.querySelector('.active-filter')
 let completedBtnFilter = document.querySelector('.completed-filter')
 
 let tasks = []
-
+let error = false
 const v2 = () => {
     return Number(new Date).toString(16)
 }
@@ -21,22 +21,46 @@ const removeTask = (id) => {
     render(tasks)
 }
 
-const render = (tasks) => {
-    list.innerHTML = tasks.reduce((prev, item) => `<li><input ${item.isDone ? 'checked' : ''} type="checkbox"/>${item.title}<button onclick=removeTask(${item.id})>x</button></li>` + prev, '')
+
+const changeIsDone = (id) => {
+    let task = tasks.find(t => t.id === id)
+    task.isDone = !task.isDone
+    render(tasks)
 }
 
 
-addBtn.addEventListener('click', () => {
-    addInput.value && tasks.push({id: v1(), title: addInput.value, isDone: Math.random() > 0.5})
-    console.log(tasks)
+const render = (tasks) => {
+    let div = document.createElement('div')
+    if (error && !document.querySelector('.error')) {
+        div.classList.add('error')
+        div.innerHTML = `field is required`
+        list.before(div)
+        addInput.classList.add('error-message')
+    }
+    list.innerHTML = tasks.reduce((prev, item) => `<li class=${item.isDone ? 'is-done' : ''}><input onchange=changeIsDone(${item.id}) ${item.isDone ? 'checked' : ''} type="checkbox"/>${item.title}<button onclick=removeTask(${item.id})>x</button></li>` + prev, '')
+}
+
+
+addBtn.addEventListener('click', (e) => {
+    if (!addInput.value) {
+        error = true
+        render(tasks)
+        return
+    }
+    addInput.value && tasks.push({id: v1(), title: addInput.value, isDone: false})
     addInput.value = ''
     render(tasks)
 })
 
 addInput.addEventListener('keydown', (e) => {
     if (e.code === 'Enter') {
-        addInput.value && tasks.push({id: v1(), title: addInput.value, isDone: Math.random() > 0.35})
-        console.log(tasks)
+        if (!addInput.value) {
+            error = true
+            render(tasks)
+            return
+        }
+        error = false
+        addInput.value && tasks.push({id: v1(), title: addInput.value, isDone: false})
         addInput.value = ''
         render(tasks)
     }
@@ -64,7 +88,7 @@ allBtnFilter.addEventListener('click', () => {
     allBtnFilter.classList.add('active')
     render(tasks)
 })
-
+render(tasks)
 /*?-----------------------------------*/
 
 
